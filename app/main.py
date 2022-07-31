619ea45569725a5886a89768977c6b835929b3b1
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2022-07-31 17:23:00 +0200

    added config in yaml

--- app/main.py
+++ app/main.py
@@ -1,13 +1,43 @@
 from typing import Union
-
 from fastapi import FastAPI
+from pydantic import BaseModel
+import yaml
+
+
+class Item(BaseModel):
+    name: str
+    price: float
+
 
 app = FastAPI()
 
 
+def read_config(filename="config.yaml"):
+    with open(filename) as f:
+        data = yaml.load(f)
+    print(data)
+    return data
+
+
 @app.get("/")
 def read_root():
-    return {"Hello": "World"}
+    read_config()
+    return {"Under": "Construction"}
+
+
+@app.post("/de-identify")
+def deidentify(item: Item):
+    return {"Under": "Construction"}
+
+
+@app.post("/pseudonymize")
+def deidentify():
+    return {"Under": "Construction"}
+
+
+@app.post("/de-pseudonymize")
+def deidentify():
+    return {"Under": "Construction"}
 
 
 @app.get("/items/{item_id}")
