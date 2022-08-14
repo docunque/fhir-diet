a8d8205aa26af05c35a4af7d35dc168f978c2305
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2022-08-14 18:52:59 +0200

    added cli

---
+++ app/cli.py
@@ -0,0 +1,48 @@
+from rich import print
+import sys
+import os
+import json
+import typer
+
+app = typer.Typer()
+
+
+def read_file(filename: str):
+    try:
+        with open(filename, 'r') as jfile:
+            json_data = json.load(jfile)
+            # python -m rich.emoji
+            print(f":thumbs_up: json {filename} read")
+            return json_data
+    except IOError as e:
+        print(
+            f":sad_but_relieved_face: File {filename} does not exist.")
+        print(e)
+        sys.exit(os.EX_OSFILE)
+    except ValueError as e:
+        print(
+            f":sad_but_relieved_face: Cannot parse json data.")
+        print(e)
+        sys.exit(os.EX_OSFILE)
+
+
+@app.command()
+def deidentify(resource_filename: str):
+    resource = read_file(resource_filename)
+    print(f"Hello {resource_filename}")
+
+
+@app.command()
+def pseudonymize(resource_filename: str):
+    resource = read_file(resource_filename)
+    print(f"Hello {resource_filename}")
+
+
+@app.command()
+def depseudonymize(resource_filename: str):
+    resource = read_file(resource_filename)
+    print(f"Hello {resource_filename}")
+
+
+if __name__ == "__main__":
+    app()
