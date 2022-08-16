755e125f97068baea77c7248b1e363801e3271b2
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2022-08-16 14:59:16 +0200

    CORS

--- app/main.py
+++ app/main.py
@@ -21,8 +21,6 @@ def get_settings():
 app = FastAPI()
 
 origins = [
-    "http://localhost.tiangolo.com",
-    "https://localhost.tiangolo.com",
     "http://localhost",
     "http://localhost:8080",
 ]
