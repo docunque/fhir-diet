30b1a05ddcad7ae7e63e29e0a0ec8ff86ac3d514
Lorenzo Bracciale <lorenzo.bracciale@gmail.com>
2022-08-16 15:14:58 +0200

    improved test on deidentify

--- app/test/test_deidentify.py
+++ app/test/test_deidentify.py
@@ -3,16 +3,24 @@ from config import Settings
 from cli import read_file
 from config import Settings
 from deidentify import perform_deidentification
+import copy
 
 
 class TestDeidentify(unittest.TestCase):
-    def test_initial(self):
+    def test_redact(self):
         # initial test case
         settings = Settings()
+        settings.rules = [
+            {'match': "TODO-SELECT-PATIENT-NAME",
+             'action': 'redact'}
+        ]
         resource = read_file('test/fhir/simple_patient.json')
         print("Resource loaded: ", resource)
-        ret = perform_deidentification(resource, settings)
-        print(ret)
+        expected = copy.copy(resource)
+        del expected['name']  # redact the name attribute
+        result = perform_deidentification(resource, settings)
+        print(result)
+        self.assertDictEqual(expected, result)
 
 
 if __name__ == '__main__':
