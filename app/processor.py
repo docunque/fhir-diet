2494954140c13bb85207042b79465873e01b133e
EmanueleRaso <emanuele.raso@gmail.com>
2022-12-11 15:26:08 +0100

    Fixed multiple FHIR resources

--- app/processor.py
+++ app/processor.py
@@ -6,10 +6,12 @@ from pseudonymize import actions as pseudo_actions, perform_pseudonymization
 from depseudonymize import actions as depseudo_actions, perform_depseudonymization
 
 
-def process_data(resource, settings):
+def _process_single_resource(resource, settings):
     for rule in settings.rules:
-        fhirpathpy.engine.invocations['log'] = {'fn': lambda ctx, els: [{'path': x.path, 'value': x.data} for x in els]}
-        matched_elements = fhirpathpy.evaluate(resource, rule['match'] + '.log()', [])
+        fhirpathpy.engine.invocations['log'] = {'fn': lambda ctx, els: [
+            {'path': x.path, 'value': x.data} for x in els]}
+        matched_elements = fhirpathpy.evaluate(
+            resource, rule['match'] + '.log()', [])
         for el in matched_elements:
             action = rule['action']
             params = rule['params'] if 'params' in rule.keys() else {}
@@ -18,7 +20,14 @@ def process_data(resource, settings):
             elif action in list(pseudo_actions.keys()):
                 result = perform_pseudonymization(action, resource, el, params)
             elif action in list(depseudo_actions.keys()):
-                result = perform_depseudonymization(action, resource, el, params)
+                result = perform_depseudonymization(
+                    action, resource, el, params)
             else:
                 not_implemented(f'Method {action} is not implemented')
     return result
+
+
+def process_data(resource, settings):
+    if isinstance(resource, list):
+        return [_process_single_resource(res, settings) for res in resource]
+    return _process_single_resource(resource, settings)
